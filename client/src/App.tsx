b8d583beccbad53e2deed6e2cff35d717825731b
whesp24 <whesp24@gmail.com>
2026-04-21 17:12:58 -0700

    App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,7 +13,6 @@ import JapanMarket from "@/pages/JapanMarket";
 import NewsFeed from "@/pages/NewsFeed";
 import StockDetail from "@/pages/StockDetail";
 import XVoices from "@/pages/XVoices";
-import Networking from "@/pages/Networking";
 import Network  from "@/pages/Network";
 import Projects from "@/pages/Projects";
 import { createContext, useContext, useEffect, useState, useRef, useCallback } from "react";
